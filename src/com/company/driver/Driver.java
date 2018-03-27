e42940b1aae14f9078d264c3b9b21d6e6d4707f3
Rom <rsvalme@wpi.edu>
2018-03-27 02:26:21 -0400

    adding input handler validation

--- src/com/company/driver/Driver.java
+++ src/com/company/driver/Driver.java
@@ -8,11 +8,13 @@ import java.util.Collections;
 import com.company.airport.Airport;
 import com.company.airport.Airports;
 import com.company.dao.ServerInterface;
+import com.company.handler.InputHandler;
 
 /**
  * @author blake
  *
  */
+//Need to: InputHandler asking user to input data in sequence
 public class Driver {
 
 	/**
@@ -24,6 +26,8 @@ public class Driver {
 	 * @param args is the arguments passed to java vm in format of "CS509.sample teamName" where teamName is a valid team
 	 */
 	public static void main(String[] args) {
+
+		//TODO list of airports validate airport codes or strings???
 		if (args.length != 1) {
 			System.err.println("usage: CS509.sample teamName");
 			System.exit(-1);
@@ -32,10 +36,14 @@ public class Driver {
 		
 		String teamName = args[0];
 		// Try to get a list of airports
+		/*
 		Airports airports = ServerInterface.INSTANCE.getAirports(teamName);
 		Collections.sort(airports);
 		for (Airport airport : airports) {
 			System.out.println(airport.toString());
 		}
+		*/
+		InputHandler inpH1 = new InputHandler();
+		inpH1.validateDepartureDate();
 	}
 }
---
+++ src/com/company/handler/InputHandler.java
@@ -0,0 +1,139 @@
+package com.company.handler;
+import java.util.Scanner;
+import com.company.airport.Airports;
+import com.company.airport.Airport;
+// Done: Finished reading input capability for user specified options.
+// Need to: Validate User data
+//          Write script that gets info from user, checks if info is valid, and stores.
+public class InputHandler {
+    String departure_airport_code;
+    String arrival_airport_code;
+    String departure_date;
+    String arrival_date;
+    String trip_type;
+    String seat_type;
+
+    public void validateAirportCode(Airports airports){
+        // need the list of airports
+        // check what information is returned by the airport
+        // run read from here
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        int found_equal_dep_air = 0;
+        int found_equal_arr_air = 0;
+        while(found_equal_dep_air == 0) {
+            System.out.println("Enter the departure airport code number: ");
+            String dep_airport_code = reader.nextLine(); // Scans the line for the string.
+            //compare all airports to make sure entered string is in the list
+            for (Airport airport : airports) {
+                //TODO MAKE SURE THE AIRPORTS CANT EQUAL
+                if (airport.code().equals(dep_airport_code)) {
+                    found_equal_dep_air = 1;
+                    this.departure_airport_code = dep_airport_code;
+                }
+            }
+            if (found_equal_dep_air == 0) {
+                System.out.println("Airport not found!");
+            }
+        }
+        //once finished
+        while(found_equal_arr_air == 0) {
+            System.out.println("Enter the arrival airport code number: ");
+            String arr_airport_code = reader.nextLine(); // Scans the line for the string.
+            //compare all airports to make sure entered string is in the list
+            for (Airport airport : airports) {
+                if (airport.code().equals(arr_airport_code)) {
+                    found_equal_arr_air = 1;
+                    this.arrival_airport_code = arr_airport_code;
+                }
+            }
+            if (found_equal_arr_air == 0) {
+                System.out.println("Airport not found!");
+            }
+        }
+        reader.close();
+        //System.out.println(this.arrival_airport_code);
+        //System.out.println(this.departure_airport_code);
+}
+
+    public void validateTripType(){
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        int found_trip_type = 0;
+        while(found_trip_type == 0) {
+            System.out.println("Enter the desired trip type (round trip or one-way): ");
+            String t_type = reader.nextLine(); // Scans the line for the string.
+            //compare all airports to make sure entered string is in the list
+            if (t_type.equals("round trip") || t_type.equals("one-way")) {
+                    found_trip_type = 1;
+                    this.trip_type = t_type; // Scans the line for the string.
+                }
+            if (found_trip_type == 0) {
+                System.out.println("Trip type not found!");
+            }
+        }
+        reader.close();
+   }
+
+    public void validateDepartureDate(){
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        int found_dep_date = 0;
+        while(found_dep_date == 0){
+            System.out.println("Enter the desired departure date (e.g. format 05_05_2018): ");
+            String dep_date = reader.nextLine(); // Scans the line for the string.
+            String [] sdep_date = dep_date.split("_");
+            if(sdep_date.length != 3){
+                continue;
+            }
+            int month = Integer.parseInt(sdep_date[0]);
+            int day = Integer.parseInt(sdep_date[1]);
+            int year = Integer.parseInt(sdep_date[2]);
+            if(month == 5 && year == 2018 && day <= 31){
+                // has to be between in May
+                this.departure_date = dep_date;
+                found_dep_date = 1;
+            }
+        }
+        reader.close();
+    }
+
+    public void readArrivalDate(){
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        int found_arr_date = 0;
+        while(found_arr_date == 0){
+            System.out.println("Enter the desired arrival date (e.g. format 05_05_2018): ");
+            String arr_date = reader.nextLine(); // Scans the line for the string.
+            String [] sarr_date = arr_date.split("_");
+            if(sarr_date.length != 3){
+                continue;
+            }
+            int month = Integer.parseInt(sarr_date[0]);
+            int day = Integer.parseInt(sarr_date[1]);
+            int year = Integer.parseInt(sarr_date[2]);
+            if(month == 5 && year == 2018 && day <= 31){
+                // has to be between in May
+                this.arrival_date = arr_date;
+                found_arr_date = 1;
+            }
+        }
+        reader.close();
+    }
+
+    public void validateSeatType(){
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        int found_seat_type = 0;
+        while(found_seat_type == 0) {
+            System.out.println("Enter the desired trip type (coach or first class): ");
+            String s_type = reader.nextLine(); // Scans the line for the string.
+            //compare all airports to make sure entered string is in the list
+            if (s_type.equals("coach") || s_type.equals("first class")) {
+                found_seat_type = 1;
+                this.seat_type = s_type; // Scans the line for the string.
+            }
+            if (found_seat_type == 0) {
+                System.out.println("Seat type not found!");
+            }
+        }
+        reader.close();
+    }
+
+
+}
